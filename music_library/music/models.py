3886cb918213ec1d0cb2407189e8f4b61d5f345a
Matt Taylor <89653410+Matt-T977@users.noreply.github.com>
2021-10-01 14:31:11 -0500

    Song Model Created with serializer / GET and POST views created in class SONGLIST
    Song Model Created with serializer / GET and POST views created in class SONGLIST

--- music_library/music/models.py
+++ music_library/music/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.db.models.fields import CharField
 
 # Create your models here.
+class Song(models.Model):
+    title = CharField(max_length=50)
+    artist = CharField(max_length=50)
+    album = models.CharField(max_length=50)
+    release_date = models.DateTimeField()
--- music_library/music/views.py
+++ music_library/music/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from .models import Song
+from .serializers import SongSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import serializers, status
+
 
 # Create your views here.
+
+class SongList(APIView):
+
+
+    def get(self, request):
+        song = Song.objects.all()
+        serializer = SongSerializer(song, many=True)
+        return Response(serializers.data)
+
+    def post(self, request):
+        serializer = SongSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)        
